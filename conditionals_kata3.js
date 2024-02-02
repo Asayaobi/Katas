7471d8014d02cf18dbea3ce8fa494a595cde89fd
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-02 23:40:11 +0700

    Update conditionals_kata3.js

--- conditionals_kata3.js
+++ conditionals_kata3.js
@@ -1,36 +1,26 @@
-let temperature
-let weather
 function categorizeDay(temperature, weather) {
-// If the temperature is 25 degrees or above and it's sunny, it's a "Beach day".
-// If it's sunny but the temperature is below 25 degrees, it's a "Park day".
-    if (temperature >= 25){
-        if (weather === 'sunny'){
+    if (temperature >= 25) {
+        if (weather === 'sunny') {
             return 'Beach day'
         } else {
             return 'Casual day'
         }
-    } else if (temperature < 25){
+    } else if (temperature >= 15) {
         if (weather === 'sunny') {
             return 'Park day'
+        } else if (weather === 'rainy') {
+            return 'Museum day'
         } else {
             return 'Casual day'
         }
-    }
-// If it's rainy and the temperature is below 15 degrees, it's a "Stay home" day.
-// If it's rainy but the temperature is 15 degrees or above, it's a "Museum day".
-// In all other cases, it's a "Casual day".
-    else if (temperature < 15){
-        if  (weather === 'rainy'){
+    } else if (temperature < 15) {
+        if (weather === 'sunny') {
+            return 'Park day'
+        } else if (weather === 'rainy') {
             return 'Stay home'
         } else {
             return 'Casual day'
         }
-    } else if (temperature >= 15){
-        if(weather === 'rainy') {
-            return 'Museum day'
-        }
-    } else {
-        return 'Casual day'
     }
 }
 
