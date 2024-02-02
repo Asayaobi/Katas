061c9309cfd47812cb3960b50e3d7794255ba067
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-02 16:24:39 +0700

    Create logicalops_kata1.js

---
+++ logicalops_kata1.js
@@ -0,0 +1,12 @@
+function checkCredentials(username, password) {
+    if (username === 'admin' && password === '12345') {
+        return 'granted'
+        } else {
+            return 'denied'
+        }
+        return 'denied'
+    }
+console.log(checkCredentials('admin', '12345'))
+console.log(checkCredentials('admin', '0000'))
+console.log(checkCredentials('user', '12345'))
+console.log(checkCredentials('user', '0000'))
