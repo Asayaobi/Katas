fbb4951a8d9502c2bc6138cc015e68696efe92f5
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-02 15:10:53 +0700

    Assigned booking days variable

--- conditionals_kata2.js
+++ conditionals_kata2.js
@@ -1,12 +1,12 @@
-let bookingDays = 31
 let output = ''
+let bookingDays = 8
 if (bookingDays < 30) {
   output = 'Your booking is being processed'
 }
 else {
     output = 'Please contact us for monthly bookings'
 }
-return output
+// return output
 
 
 console.log(output)
