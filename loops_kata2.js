c1f372c9758583b39e8abdd763dc7417a7531cda
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-05 14:03:37 +0700

    Update loops_kata2.js

--- loops_kata2.js
+++ loops_kata2.js
@@ -2,29 +2,29 @@
 // The function, named extractAndSort, will take two parameters: an array of numbers and a numerical value n indicating how many elements to extract.
 // Before returning the output array, convert it into a string.
 
+// let arrayString = String([0,9,9])
+// console.log(String([3,2,1]))
+// console.log(arrayString)
 
-// function extractAndSort(numbers,n) {
+// this one also works
+// function extractAndSort(numbers, n) {
 // let result = []
-//     for (let i = n - 1; i >= 0; i = i - 1){
-//         result.push(String(numbers[i]))
+//     for (i = n - 1; i >= 0; i = i - 1) {
+//         result.push(numbers[i])
 //     }
-//     return result
+//     return String(result)
 // }
 
-function extractAndSort(numbers, n) {
-let result = []
-    for (i = n - 1; i >= 0; i = i - 1) {
-        result.push(numbers[i])
+
+function extractAndSort (numbers, n) {
+    let result = []
+    let c = n - 1
+    for (let i = 0; i <= n - 1; i = i + 1) {
+        result[i] = numbers[c]
+        c = c - 1
     }
     return String(result)
 }
-
 console.log(extractAndSort([1, 2, 3, 4, 5, 6], 3))
 console.log(extractAndSort([10, 20, 30, 40, 50, 60], 4))
 console.log(extractAndSort([5, 15, 20], 2))
-
-
-// let arrayString = String([0,9,9])
-
-// console.log(String([3,2,1]))
-// console.log(arrayString)
