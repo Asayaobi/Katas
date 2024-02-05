cbe9df3f835fe972c523c686885b6c5ea0c4fdf7
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-05 13:01:59 +0700

    Update loops_kata2.js

--- loops_kata2.js
+++ loops_kata2.js
@@ -1,4 +1,4 @@
-// Your task is to write a JavaScript function that uses a "for" loop to extract the first n elements from an array of numbers and return them in descending order.
+// write function that uses a "for" loop to extract the first n elements from an array of numbers and return them in descending order.
 // The function, named extractAndSort, will take two parameters: an array of numbers and a numerical value n indicating how many elements to extract.
 // Before returning the output array, convert it into a string.
 
@@ -12,13 +12,19 @@
 // }
 
 function extractAndSort(numbers, n) {
-let result = ''
-    for (i = n -1; i <= 0; i - 1) {
-        result = result + String(numbers[i])
+let result = []
+    for (i = n -1; i >= 0; i = i - 1) {
+        result.push(String(numbers[i]))
     }
-    return result
+    return String(result)
 }
 
 console.log(extractAndSort([1, 2, 3, 4, 5, 6], 3))
 console.log(extractAndSort([10, 20, 30, 40, 50, 60], 4))
 console.log(extractAndSort([5, 15, 20], 2))
+
+
+// let arrayString = String([0,9,9])
+
+// console.log(String([3,2,1]))
+// console.log(arrayString)
