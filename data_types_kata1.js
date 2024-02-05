6698efd711d7ecb474c5ffa5c1c3eca18dbdfb37
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-05 10:38:02 +0700

    Update data_types_kata1.js

--- data_types_kata1.js
+++ data_types_kata1.js
@@ -1,6 +1,9 @@
+// The function should check if both parameters are numbers. If they are, it should apply the discount to the price and return the new discounted price.
+// If either parameter is not a number, the function should return the error message: "Error: Invalid input"
+
 function addDiscount(price, discount) {
-    if (typeof price == 'number' && typeof discount == 'number') {
-        return price * discount
+    if (typeof price === 'number' && typeof discount === 'number') {
+        return price - (price * discount)
     } else {
         return 'Error: Invalid input'
     }
