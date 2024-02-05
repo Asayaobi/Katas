98c66e4b25c2522846d5d02e1eb26cb173a46ba0
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-05 12:37:12 +0700

    Create nest_kata3.js

---
+++ nest_kata3.js
@@ -0,0 +1,19 @@
+function isEven(a) {
+    if (a % 2 === 0) {
+        return true
+    }
+}
+
+function sumEvenNumbers(numbers) {
+let sum = 0
+    for (let num of numbers) {
+        if (isEven(num)) {
+            sum = sum + num
+        }
+    }
+    return sum
+}
+
+console.log(sumEvenNumbers([1, 20, 5, 4]))
+console.log(sumEvenNumbers([0, 100, -20, 5.5, -10]))
+console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6]))
