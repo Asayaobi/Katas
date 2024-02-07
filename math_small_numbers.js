eb0afaead2024c967e970dd8df3e66ead0024e86
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-07 13:14:49 +0700

    math kata1

---
+++ math_small_numbers.js
@@ -0,0 +1,29 @@
+// Create a JavaScript function called findSmallestNumber.
+// This function should take between 2 and 4 numbers as its parameters and return the smallest number.
+// Use the correct Math method.
+
+// this one works for 4 numbers
+// function findSmallestNumber(a, b, c, d) {
+//     let smallest = Math.min(a, b, c, d)
+//     return smallest
+// }
+
+function findSmallestNumber(a, b, c, d) {
+let smallest
+    if (c === undefined && d === undefined) {
+        smallest = Math.min(a, b)
+        return smallest
+    } else if (d === undefined) {
+        smallest = Math.min(a, b, c)
+        return smallest
+    } else if (a && b && c && d) {
+        smallest = Math.min(a, b, c, d)
+        return smallest
+    } else {
+        return 'please pass 2- 4 numbers'
+    }
+}
+
+console.log(findSmallestNumber(2,71,5,3.9));
+console.log(findSmallestNumber(20,80,-5.5));
+console.log(findSmallestNumber(8,1));
