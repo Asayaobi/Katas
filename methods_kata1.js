c54605f45c2b182fbf165d0713c6406b41ec01f0
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-07 11:44:56 +0700

    complete delete kata1

---
+++ methods_kata1.js
@@ -0,0 +1,12 @@
+// Create an object greeter and add a method sayHello to it.
+// The method should return 'Hello World'
+
+// greeter.sayHello() // 'Hello World'
+
+let greeter = {
+    sayHello: function() {
+        return 'Hello World'
+    }
+}
+
+console.log(greeter.sayHello());
