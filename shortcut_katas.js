184183ae315a00ceb10792b29a8fdc89e097cb41
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-08 11:23:28 +0700

    complete ternary

--- shortcut_katas.js
+++ shortcut_katas.js
@@ -2,13 +2,13 @@
 // This function should take a single parameter, a number, and use the decrement operator to decrease the value of the number by one.
 // The function should then return the decremented value.
 
-// function decrementValue(number) {
-//     number--
-//     return number
-// }
+function decrementValue(number) {
+    number--
+    return number
+}
 
-// console.log(decrementValue(5))
-// console.log(decrementValue(100));
+console.log(decrementValue(5))
+console.log(decrementValue(100));
 
 // Replace the sum calculation in the following code with the Addition Assignment operator.
 // Don't delete the variable c.
@@ -23,17 +23,56 @@ console.log(addTen(10));
 
 // In the following function, convert the if condition into a ternary operator.
 
-// function numerToBoolean(n) {
-//     if (n > 0) {
-//         return true
-//     } else {
-//         return false
-//     }
-// }
+// Use a shortcut in the following function to replace the increment of i in the loop and the increment of sum.
+function timer() {
+    let sum = 0
+    for (let i = 0; i < 10; i++) {
+      sum++
+    }
+    return sum
+  }
+
+  console.log(timer());
+
+
+
+
+
+function numerToBoolean(n) {
+    if (n > 0) {
+        return true
+    } else {
+        return false
+    }
+}
 
 function numberToBoolean(n) {
-   let ntoBoolean = n > 0 ? true : false
+    return n > 0 ? true : false
 }
+
+
 
 console.log(numberToBoolean(5));
 console.log(numberToBoolean(-30))
+
+// Halfordouble
+// Create a function halfOrDouble that takes an array of numbers.
+// The function should then check the value of each number and, if it's less than 10, it should double it, otherwise it should halve it.
+// The function should then return the updated array.
+// Use a ternary operator.
+
+
+
+  function halfOrDouble1(numbers) {
+        let output = []
+        let newnumber = 0
+        let c = 0
+        for (let number of numbers) {
+            newnumber = number <= 10 ? number *= 2 : number /= 2
+            output[c] = newnumber
+            c++
+        }
+        return output
+    }
+
+console.log(halfOrDouble1([10,5,2,14]))
