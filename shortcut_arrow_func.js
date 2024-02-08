f4db2c6637aaa278266d5e8aba9634f0f5206cfd
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-08 11:27:28 +0700

    Create shortcut_arrow_func.js

---
+++ shortcut_arrow_func.js
@@ -0,0 +1,10 @@
+// Convert the following function into an arrow function.
+
+// function add(a, b) {
+//     return a + b
+// }
+
+const add = (a, b) => a + b
+
+console.log(add(5,2));
+console.log(add(1,2));
