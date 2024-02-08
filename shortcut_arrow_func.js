492ab0bdac40b2df00bd3cd72523fd8af509f236
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-08 11:33:28 +0700

    complete function kata1-3

--- shortcut_arrow_func.js
+++ shortcut_arrow_func.js
@@ -8,3 +8,19 @@ const add = (a, b) => a + b
 
 console.log(add(5,2));
 console.log(add(1,2));
+
+// The following arrow function is broken. Can you fix it?
+// const subtract (a, b) => a - b
+
+const subtract = (a, b) => a - b
+
+console.log(subtract(5,2));
+console.log(subtract(10,1));
+
+// The following arrow function is broken. Can you fix it?
+// const multiply = a, b => a * b
+
+const multiply = (a, b) => a * b
+
+console.log(multiply(10,2));
+console.log(multiply(9,3));
