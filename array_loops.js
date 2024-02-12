dde27520e916d997ffc8c311cba22de395039a6a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-12 10:54:55 +0700

    Update array_loops.js

--- array_loops.js
+++ array_loops.js
@@ -84,19 +84,83 @@ console.log(filterOutNegativeNumbers([1,2,3]));
 console.log(filterOutNegativeNumbers([1,-2,-3,4]));
 console.log(filterOutNegativeNumbers([-10,-20,-30,-40]));
 
+// You are given a function that is intended to return an array of strings that are longer than 4 characters.
+// However, there's a mistake in the implementation of the filter() method.
+// Your task is to identify and correct the error in the function.
+function getLongStrings(strings) {
+    const longStrings = strings.filter(string => string.length > 4)
+    return longStrings
+}
+console.log(getLongStrings(['Hello','Hola','Ciao']));
+console.log(getLongStrings(['Grazie','Merci','Tak']));
+console.log(getLongStrings(['Grazie','Merci','Tak','Kop Khun']));
+
+// Create a JavaScript function named filterAndSquareEvens. 
+// This function should take an array of numbers as its input. 
+// It should first filter out all odd numbers, and then square each of the remaining even numbers. 
+// The function should return a new array with these squared even numbers.
+function filterAndSquareEvens(numbers) {
+    const evenNumbers = numbers.filter((num) => {return num % 2 === 0})
+    const squareNumbers = evenNumbers.map(evenNumber => evenNumber * evenNumber)
+    return squareNumbers
+}
 
 
+console.log(filterAndSquareEvens([1,2,3,4,5,6]));
+console.log(filterAndSquareEvens([10,-10,20,-20]));
+console.log(filterAndSquareEvens([0,11,22,0]));
 
-const numbersA = [1, 2, 3, 4, 5, 6];
-const evenNumbers = numbersA.filter(number => number % 2 === 0)
-console.log(evenNumbers);
+//find()
+// You are provided with a function that takes an array of objects (representing people) and a name (string).
+// The function returns the first person object whose name matches the provided name.
+// However, this function currently uses a for loop.
+// Your task is to refactor this function to use the find() method instead.
+// function findPerson(people, name) {
+//     for (let i = 0; i < people.length; i++) {
+//         if (people[i].name === name) {
+//             return people[i]
+//         }
+//     }
+// }
+
+function findPerson(people, name) {
+    const personFound = people.find(people => people.name === name)
+    return personFound
+}
+
+console.log(findPerson([{'name':'Alice','age':25},{'name':'Alice','age':30}],'Alice'));
+console.log(findPerson([{'name':'Alice','age':25},{'name':'Bob','age':37}],'Alice'));
+console.log(findPerson([{'name':'Bob','age':37},{'name':'Alice','age':25}],'Alice'));
+
+// Write a JavaScript function named findProductById.
+// This function will take an array of product objects and a specific product ID.
+// It should use the find() method to search through the array and return the product object that has the matching ID.
+// If no product with the given ID is found, the function should return null.
+
+// 1.find product
+// const findProduct = products.find((products) => {return products.id === productID})
+// 2. if findProduct is truty return findProduct, if it's falsy null
+// if (findProduct) {
+//     return findProduct
+// } else {
+//     return null
+// }
+
+function findProductById(products, productID) {
+    const findProduct = products.find(products => products.id === productID)
+    const result = findProduct ? findProduct : null
+    return result
+}
+console.log(findProductById([{'id':1,'name':'Smartphone'},{'id':2,'name':'Laptop'},{'id':3,'name':'Tablet'}],2));
+console.log(findProductById([{'id':109,'name':'Keyboard'},{'id':107,'name':'Duck'}],109));
+console.log(findProductById([{'id':109,'name':'Keyboard'},{'id':107,'name':'Duck'}],20));
 
 
-const products = [
-    { name: 'Apple', category: 'fruit' },
-    { name: 'Carrot', category: 'vegetable' },
-    { name: 'Banana', category: 'fruit' }
- ]
+// const products = [
+//     { name: 'Apple', category: 'fruit' },
+//     { name: 'Carrot', category: 'vegetable' },
+//     { name: 'Banana', category: 'fruit' }
+//  ]
 //  const fruits = []
 //  for (i = 0; i < products.length ; i++) {
 //     if (products[i].category === 'fruit') {
@@ -117,26 +181,21 @@ const truthyValues = values.filter(word => Boolean(word))
 // }
 console.log(truthyValues);
 
-// Create a JavaScript function named filterAndSquareEvens. This function should take an array of numbers as its input. It should first filter out all odd numbers, and then square each of the remaining even numbers. The function should return a new array with these squared even numbers.
-let allnumbers = [1,2,3,4,5,6]
 
-const filterEven = allnumbers.filter(n => n % 2 === 0)
-const filterAndSquareEvens = filterEven.map(even => even * even)
-console.log(filterAndSquareEvens);
 
-const people = [{'name':'Alice','age':25},{'name':'Alice','age':30}]
-const name = 'Alice'
+// const people = [{'name':'Alice','age':25},{'name':'Alice','age':30}]
+// const name = 'Alice'
 
-const findPerson = people.find(person => person.name === name)
-console.log(findPerson);
+// const findPerson = people.find(person => person.name === name)
+// console.log(findPerson);
 
 // Write a JavaScript function named replaceProductPrice.
 // This function should take two parameters: an array of product objects and a product ID.
 // Each product object contains an id, a name, and a price.
 // The function should find the product with the given ID and increase its price by 10%.
 // The function should return a new array with the updated product objects.
-const productsA = [{'id':1,'price':100},{'id':2,'price':200}]
-let productID = 1
+// const productsA = [{'id':1,'price':100},{'id':2,'price':200}]
+// let productID = 1
 
 // const findProductPriceA = productsA.find(pA => pA.id === productID)
 // const findProductPriceB = productsA.find( pB => pB.id !== productID)
@@ -151,12 +210,12 @@ let productID = 1
 // modify the price product.price = product.price * 1.1
 //return new array called replaceProductPrice with updated products
 
-function replaceProductPrice(products, productID) {
-    for (let product of products){
-        if (product.id === productID) {
-            product.price *= 1.1
-        }
-    }
-    return products
-}
-console.log(replaceProductPrice([{'id':1,'price':100},{'id':2,'price':200}],2));
+// function replaceProductPrice(products, productID) {
+//     for (let product of products){
+//         if (product.id === productID) {
+//             product.price *= 1.1
+//         }
+//     }
+//     return products
+// }
+// console.log(replaceProductPrice([{'id':1,'price':100},{'id':2,'price':200}],2));
