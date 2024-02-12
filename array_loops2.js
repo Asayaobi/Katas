f9a3518682f87b3dd60e7dafb080aed1cebeea59
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-12 14:57:51 +0700

    Update array_loops2.js

--- array_loops2.js
+++ array_loops2.js
@@ -53,3 +53,40 @@ console.log(checkAndTransform([2,3,4],10));
 // This function will take two parameters: an array of students scores and a numerical threshold.
 // It should check whether all numbers in the array are greater than the given threshold (all students have passed the exam).
 // The function should return true if all numbers meet this criterion, or false otherwise.
+function allPass(scores, passingScore) {
+    const failStudent = scores.some(score => score < passingScore)
+    return !failStudent
+}
+console.log(allPass([7,8,8,6,10],5));
+console.log(allPass([7,8,8,2],5));
+console.log(allPass([10,7,8,10,10,9],5));
+
+// Create a JavaScript function called searchHouses.
+// This function should take 3 parameters: an array of houses, a number (price) and a string (location).
+// It should return true of all the houses contain the location string in the name of their location 
+// and if they all below the price; otherwise return false.
+
+// 1. find location 
+// 2. if there's location found, search for the price
+// 3. if all of the prices below the given price, return true, else return false
+
+// function searchHouses(houses, givenPrice, givenPlace) {
+//     const filterLocation = houses.every(house => house.location.includes(givenPlace))
+//     if (filterLocation) {
+//         const priceAndPlace = houses.every(p => p.price < givenPrice)
+//         return priceAndPlace
+//     } else {
+//         return false
+//     }
+// }
+function searchHouses(houses, givenPrice, givenPlace) {
+    const filterLocation = houses.every(house => house.location.includes(givenPlace))
+    const filterPrice = filterLocation ? houses.every(p => p.price < givenPrice) : false
+    return filterPrice
+}
+console.log(searchHouses([{'price':100,'location':'Koh Phangan'},{'price':50,'location':'Koh Samui'}],150,'Koh'))//true
+console.log(searchHouses([{'price':100,'location':'Koh Phangan'},{'price':50,'location':'Koh Samui'}],150,'Bali'))//false
+console.log(searchHouses([{'price':100,'location':'Koh Phangan'},{'price':50,'location':'Koh Samui'}],90,'Koh'))//false
+console.log(searchHouses([{'price':100,'location':'Bali'},{'price':80,'location':'Koh Samui'}],90,'Bali'))//false
+
+
