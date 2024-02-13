fafb01de71955ed60a5eba27a85b21961cf10d8e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-02-13 11:20:52 +0700

    Update async.js

--- async.js
+++ async.js
@@ -1,18 +1,22 @@
 // Turn the following into an "async" function:
-// const add = async (a, b) => a + b
+// const add = (a, b) => a + b
 
-const add = async (a, b) => {
-    let add = a + b
-    await add
-}
+const add = async (a, b) => a + b
+
+console.log(add(1, 2));
 
 // The function getMessages reads data from a different server. 
 //Write the correct code so that the function waits for it before continuing.
+// const search = () => {
+//   let messages = getMessages()
+//   return messages
 
 const search = async () => {
-  let messages = getMessages()
-  await messages
+  let messages = await getMessages()
+  return messages
 }
+
+console.log(search());
 
 // The following function is wrong, fix it.
 // const filterArray = () await => {
@@ -21,53 +25,62 @@ const search = async () => {
 // }
 
 const filterArray = async () => {
-    let data = async getData()
-    await data
-  }
+  let data = await getData()
+  return data
+}
 
-//   The function getResults() returns an error. 
-//   Collect the error and log it in the console.
+// The function getResults() returns an error. 
+// Collect the error and log it in the console.
 // const search = async () => {
 //   let results = await getResults()
 // }
 
 const search = async () => {
+  try {
     let results = await getResults()
-    try {
-        getResults()
-    } catch (error) {
-        console.log("An error occurred:", error.message) 
-    }
+  } catch (error) {
+    console.log("An error occurred:", error.message)
   }
+}
+console.log(search())
 
-//   One of the 2 functions getResults() and getPages() returns an error. 
-//   You don't know which one. Collect any of the 2 errors and log them in the console.
+// One of the 2 functions getResults() and getPages() returns an error. 
+// You don't know which one. Collect any of the 2 errors and log them in the console.
+// const search = async () => {
+//   let results = await getResults()
+//   let pages = await getPages()
+// }
 
-//   const search = async () => {
-//     let results = await getResults()
-//     let pages = await getPages()
-//   }
-
-  const search = async () => {
-    let results = await getResults()
-    let pages = await getPages()
-    try {
-        getResults()
-        getPages()
-    } catch (error) {
-        console.log("An error occurred:", error.message) 
-    }
+const search = async () => {
+  let results
+  let pages
+  try {
+    results = await getResults()
+  } catch (error) {
+    console.log("An error occured while getting results data", error.message)
   }
-
-  //axios
-  const getFirstTodo = async () => {
-    let response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    return response.data
+  try {
+    pages = await getPages()
+  } catch (error) {
+    console.log("An error occured while getting pages data", error.message)
   }
-  
-  console.log(await getFirstTodo())
+}
+console.log(search());
 
-//   Create an async function getTodo that:
+// Create an async function getFirstTodo that uses axios to make a request to the url https://jsonplaceholder.typicode.com/todos/1 and returns the data of the response object.
+// Test your code on the Simulation Platform.
+
+// Test the following:
+const getFirstToDo = async () => {
+  let response = await axios.get(
+    'https://jsonplaceholder.typicode.com/todos/1'
+  );
+  return response.data;
+};
+
+console.log(await getFirstToDo());
+
+// Create an async function getTodo that:
 
 // receives a parameter number
 // uses axios to make a request to the url https://jsonplaceholder.typicode.com/todos/1 replacing the last 1 with the parameter function
@@ -75,7 +88,33 @@ const search = async () => {
 // Test your code on the Simulation Platform.
 
 // Test the following:
+// const getFirstToDo = async (n) => {
+//   let response = await axios.get(
+//     'https://jsonplaceholder.typicode.com/todos/1'
+//   );
+//   return response.data;
+// };
 
-// getFirstTodo(1) // { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
-// getFirstTodo(2) // { userId: 1, id: 2, title: 'quis ut nam facilis et officia qui', completed: false }
-// getFirstTodo(3)
+const getTodo = async (number) => {
+  let response = await axios.get(
+    'https://jsonplaceholder.typicode.com/todos/' + number
+  );
+  return response.data;
+};
+
+getTodo(1) // { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
+getTodo(2) // { userId: 1, id: 2, title: 'quis ut nam facilis et officia qui', completed: false }
+getTodo(3) // { userId: 1, id: 3, title: 'fu
+
+// Create an async function getComments that:
+// uses axios to make a request to the url https://jsonplaceholder.typicode.com/posts/1/comments
+// returns the data of the response object
+// Test your code on the Simulation Platform.
+// Test the following:
+
+const getComments = async () => {
+  let response = await axios.get(
+    'https://jsonplaceholder.typicode.com/posts/1/comments'
+  )
+  return response.data
+}
