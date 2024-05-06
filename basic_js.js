216576000da3fafa7047240124e0656e80d380ae
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-06 22:52:29 +0200

    complete kata2

--- basic_js.js
+++ basic_js.js
@@ -9,3 +9,23 @@ function maxNumber(a, b) {
 
 console.log(maxNumber(1, 2))
 console.log(maxNumber(5, 20)) 
+
+// Create a function assignGrade that takes a number score and returns a grade ('A', 'B', 'C') based on that score.
+// 'A' is for a score equal to or greater than 90.
+// 'B' is between 80 and 90.
+// 'C' is below 80.
+
+function assignGrade(score) {
+    //return grade a, b, c
+    if (score >= 90) {
+        return 'A'
+    }
+    if (score >= 80) {
+        return 'B'
+    }
+    return 'C'
+}
+
+console.log(assignGrade(90))
+console.log(assignGrade(80))
+console.log(assignGrade(70))
