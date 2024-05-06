a01d7d72bf864fcbb6de3898ba1df674edc21c49
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-06 22:20:44 +0200

    complete kata1

---
+++ basic_js.js
@@ -0,0 +1,11 @@
+// Find Maximum Number
+// Write a function maxNumber that takes two numbers a and b as arguments and returns the larger of the two.
+function maxNumber(a, b) {
+    if (a > b) {
+        return b
+    }
+    return a
+}
+
+console.log(maxNumber(1, 2))
+console.log(maxNumber(5, 20)) 
