1d34444f88a9a67a485585fcd9e816e09c83e5ac
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-11 22:22:09 +0200

    complete logical ops kata1

--- logicalops_kata1.js
+++ logicalops_kata1.js
@@ -1,9 +1,16 @@
+// Simplifying Nested If Statements
+// You have a piece of JavaScript code that uses nested if statements. 
+// Your task is to refactor this code into a single if statement using the correct logical operator 
+// to streamline the code and improve readability.
+
 function checkCredentials(username, password) {
     if (username === 'admin' && password === '12345') {
         return 'granted'
-        } 
-        return 'denied'
+    } else {
+            return 'denied'
+        }
     }
+
 console.log(checkCredentials('admin', '12345'))
 console.log(checkCredentials('admin', '0000'))
 console.log(checkCredentials('user', '12345'))
