0ddd8af2ff77c6171d6dac39f8920c9ff9249edc
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-11 23:03:33 +0200

    complete modulu kata1

--- moduluskata.js
+++ moduluskata.js
@@ -1,11 +1,16 @@
-let number = 0
-function evenOrOdd(number) {
-    if (number % 2 === 0){
+
+// Even or Odd
+// Create a function evenOrOdd that takes a number parameter 
+// and returns 'even' if the number is even, or 'odd' if the number is odd.
+
+function evenOrOdd(n) {
+    if (n % 2 === 0){
         return 'even'
     } else {
         return 'odd'
     }
 }
+
 console.log(evenOrOdd(20))
 console.log(evenOrOdd(1))
 console.log(evenOrOdd(0))
