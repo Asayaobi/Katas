3c44bfdb3ebaa4a6b63de73b29198394db274fd0
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-11 22:27:09 +0200

    complete logical ops kata2

--- logicalops_kata1.js
+++ logicalops_kata1.js
@@ -15,3 +15,20 @@ console.log(checkCredentials('admin', '12345'))
 console.log(checkCredentials('admin', '0000'))
 console.log(checkCredentials('user', '12345'))
 console.log(checkCredentials('user', '0000'))
+
+// Refactoring Adjacent If Statements
+// You have a JavaScript function that uses multiple adjacent "if" statements.
+// Your objective is to refactor this code into a single "if" statement using the correct logical operator.
+// This change aims to make the code more concise and readable.
+
+function checkRole(role) {
+    if (role === 'admin' || role === 'editor') {
+        return 'granted'
+    } else {
+        return 'denied'
+    }
+}
+
+console.log(checkRole('admin'))
+console.log(checkRole('editor'))
+console.log(checkRole('user'))
