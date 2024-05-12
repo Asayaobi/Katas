56f7275f66e4f35313a0d871af9a9a4ccf207c6a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-12 19:34:40 +0200

    complete nested kata1

--- nested_kata1.js
+++ nested_kata1.js
@@ -1,10 +1,21 @@
+// Sum of Squares
+// Your task is to create two functions.
+// The first function, calculateSquare, 
+// should take a number as a parameter and return its square.
 function calculateSquare(n) {
     return n * n
 }
-
+// The second function, sumOfSquares, 
+// should take two numbers, invoke calculateSquare for each number, and then return the sum of their squares.
 function sumOfSquares(a, b) {
-    return calculateSquare(a) + calculateSquare(b)
+    // a = calculateSquare(a)
+    // b = calculateSquare(b)
+    const sum = calculateSquare(a) + calculateSquare(b)
+    return sum
 }
+// Automatic tests are not available for this kata, please use the tests below in NodeJs.
+// sumOfSquares(5, 5) // 50
+// sumOfSquares(4, 10) // 116
 
 console.log(sumOfSquares(5, 5))
 console.log(sumOfSquares(4, 10))
