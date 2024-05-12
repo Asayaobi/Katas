f1b5a965e8a5cc7b68ed82f3fb0a1ad2686ac5f3
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-12 22:57:02 +0200

    complete oop kata3

--- nesting_kata1.js
+++ nesting_kata1.js
@@ -32,3 +32,40 @@ function calculateTotalPrice(arr) {
 
 console.log(calculateTotalPrice([{"name": "Bag", "price": 90}, {"name": "Shoes", "price": 100}]))
 console.log(calculateTotalPrice([{"name": "Car", "price": 1000}, {"name": "Apple", "price": 5}, {"name": "Laptop", "price": 500}]))
+
+// Summing Positive Numbers in Nested Arrays
+// Your task is to create a function named sumPositiveNumbers.
+// This function should take an object as its parameter.
+// Within this object, there are two properties array1 and array2, each containing an array of numbers.
+// The function should calculate the sum of all positive numbers from both arrays combined and return the total sum.
+
+let a = {
+    array1: [1, -3, 5, 7],
+    array2: [10, -2, 4, -5]
+}
+// sumPositiveNumbers(a) // 27
+
+let b = {
+    array1: [10, 20, -9],
+    array2: [200, 1]
+}
+// sumPositiveNumbers(b) // 231
+
+function sumPositiveNumbers(object) {
+    let sum = 0
+
+    for (i = 0; i < object.array1.length; i++) {
+        if (object.array1[i] > 0){
+            sum += object.array1[i]
+        }
+    }
+    for (i = 0; i < object.array2.length; i++) {
+        if (object.array2[i] > 0){
+            sum += object.array2[i]
+        }
+    }
+    return sum
+}
+
+console.log(sumPositiveNumbers(a))
+console.log(sumPositiveNumbers(b))
