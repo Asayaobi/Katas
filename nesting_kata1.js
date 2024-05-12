caf2bf29a703748da942c569f95998c65a5d2444
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-12 22:38:55 +0200

    complete oop kata1

--- nesting_kata1.js
+++ nesting_kata1.js
@@ -1,8 +1,8 @@
+// Accessing Nested Object Properties
 // Your task is to create a function named getNestedProperty.
 // This function should take an object as its parameter.
 // The object will have a nested structure, with an object inside the main object.
 // You need to access the email property inside the nested object and return its value.
-
 
 function getNestedProperty(obj) {
     return obj.contact.email
