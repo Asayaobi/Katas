6ecdf3578330c6d88d9f16a005dc4beb7f8be9f8
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-12 23:06:56 +0200

    complete oop kata4

--- nesting_kata1.js
+++ nesting_kata1.js
@@ -69,3 +69,28 @@ function sumPositiveNumbers(object) {
 
 console.log(sumPositiveNumbers(a))
 console.log(sumPositiveNumbers(b))
+
+// Player Names with Even Scores
+// Your task is to create a function named getEvenScorePlayers.
+// This function should take an array of objects as its parameter.
+// Each object represents a player with two properties: name (a string) and score (a number or a string).
+// The function should select the players whose scores are even numbers (and specifically not strings), 
+// concatenate their names into a single string with spaces, and return this string.
+
+let players1 = [{"name": "Alice", "score": 20}, {"name": "Bob", "score": 15}, {"name": "Charlie", "score": 30}, {"name": "Daisy", "score": "40"}]
+// getEvenScorePlayers(players1) // 'Alice Charlie'
+
+let players2 = [{"name": "Tom", "score": "8"}, {"name": "Tim", "score": 9}, {"name": "Tam", "score": 2}]
+// getEvenScorePlayers(players2) // 'Tam'
+function getEvenScorePlayers(players) {
+let result = ''
+    for (i = 0; i < players.length; i++) {
+        if (players[i].score % 2 === 0 && typeof players[i].score != 'string') {
+            result += players[i].name + ' '
+        }
+    }
+    return result
+}
+
+console.log(getEvenScorePlayers(players1))
+console.log(getEvenScorePlayers(players2))
