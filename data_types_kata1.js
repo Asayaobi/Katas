44194f70e7c26a9560e90d6171311f14d3d3774a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-12 17:06:05 +0200

    complete data type kata2

--- data_types_kata1.js
+++ data_types_kata1.js
@@ -18,3 +18,26 @@ console.log(addDiscount(40, 0.25))
 console.log(addDiscount('hello', 0.5))
 console.log(addDiscount(0.5, 'hello'))
 console.log(addDiscount('hello', 'hello'))
+
+// Formatting Name
+// Create a JavaScript function named formatDisplayName that takes two parameters: a user's first name and last name.
+// If the last name is not provided, the function should return the error message: "Last name missing".
+// The function should check if both parameters are strings and then return a formatted full name with a space in between.
+// If either parameter is not a string, the function should return the error message: "Error, not a string"
+
+function formatDisplayName(firstname, lastname) {
+    //if no lastname 'last name missing'
+    if (!lastname) {
+        return 'last name missing'
+        //if firstname, lastname type = string, return firstname + space + lastname
+    // if not 'not a string'
+    } else if (typeof firstname === 'string' && typeof lastname === 'string') {
+        return firstname + ' ' + lastname
+    } else {
+        return 'Error, not a string'
+    }
+}
+
+console.log(formatDisplayName('John','Doe'))
+console.log(formatDisplayName('John'))
+console.log(formatDisplayName('John',100))
