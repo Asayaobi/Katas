cdae7e799048c0b82979a33e236fb9b4bd660ecc
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-12 23:39:34 +0200

    complete oop update kata2

--- updating_kata1.js
+++ updating_kata1.js
@@ -10,6 +10,23 @@ function markAsPaid(bill) {
     bill.status = 'paid'
     return bill
 }
-
 console.log(markAsPaid(a))
 console.log(markAsPaid(b))
+
+// Mark as Value
+// Your task is to create a function called markAsValue.
+// This function should take 2 parameters: an object (a bill), and a string (a value).
+// The function should update the status property of the bill to value parameter, and then return the updated object.
+
+let c = {"total": "99", "status": "pending"}
+// markAsValue(a, "accepted") // {"total": "99", "status": "accepted"}
+let d = {"total": "71.5", "status": "pending"}
+// markAsValue(b, "denied") // {"total": "71.5", "status": "denied"}
+
+function markAsValue(bill, value) {
+    bill.status = value
+    return bill
+}
+
+console.log(markAsValue(c, "accepted"))
+console.log(markAsValue(d, 'pending'))
