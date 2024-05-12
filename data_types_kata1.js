61c32724a7692e2191bb110a78b1c6256315b7b0
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-12 18:12:56 +0200

    complete datatype kata 3

--- data_types_kata1.js
+++ data_types_kata1.js
@@ -41,3 +41,28 @@ function formatDisplayName(firstname, lastname) {
 console.log(formatDisplayName('John','Doe'))
 console.log(formatDisplayName('John'))
 console.log(formatDisplayName('John',100))
+
+// Sum Numbers in Array
+// Create a function sumNumbers that takes an array as a parameter.
+// The array can contain a combination of numbers, strings and booleans.
+// The function should find all numbers in the array, add them and return the sum.
+// If there are no numbers, the function should return 0.
+
+function sumNumbers(arr) {
+    //find all numbers in the array, add them
+    // if no numbers returns 0
+    let sum = 0
+    for (let element of arr){
+        if (typeof element === 'number'){
+            sum += element
+        }
+    }
+    return sum
+}
+
+console.log(sumNumbers([10,5]))
+console.log(sumNumbers(['10',5,'20',5]))
+console.log(sumNumbers(['hello',-20,3]))
+console.log(sumNumbers(['yes','no']))
+console.log(sumNumbers([true,'no',0.9,0.1,false,'yes']))
+console.log(sumNumbers([]))
