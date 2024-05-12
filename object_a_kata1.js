ab5587bea7dfca19fb4da3ab92467fb3131c9f38
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-12 23:35:37 +0200

    complete oop update kata1

--- object_a_kata1.js
+++
@@ -1,6 +0,0 @@
-let house = {
-    rooms: 2,
-    windows: 2,
-    bathrooms: 1,
-    material:'stone'
-}
--- updating_kata1.js
+++ updating_kata1.js
@@ -1,15 +1,15 @@
+// Mark as Paid
 // Your task is to create a function called markAsPaid.
 // This function should take an object as its only parameter (a bill), update the status property of the bill to paid, and then return the updated object.
 
 let a = {"total": "105", "status": "pending"}
-markAsPaid(a) // {"total": "105", "status": "paid"}
+// markAsPaid(a) // {"total": "105", "status": "paid"}
 let b = {"total": "33.2", "status": "pending"}
-markAsPaid(b) // {"total": "33.2", "status": "paid"}
-
-function markAsPaid(obj) {
-    obj.status = 'paid'
-    return obj
+// markAsPaid(b) // {"total": "33.2", "status": "paid"}
+function markAsPaid(bill) {
+    bill.status = 'paid'
+    return bill
 }
 
-console.log(markAsPaid({'total':'105','status':'pending'}))
-console.log(markAsPaid({'total':'33.2','status':'pending'}));
+console.log(markAsPaid(a))
+console.log(markAsPaid(b))
