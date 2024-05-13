23a52a925c10abee1558f3e075c783195581269d
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-13 23:08:45 +0200

    complete add oop kata1

--- adding_kata1.js
+++ adding_kata1.js
@@ -1,27 +1,20 @@
+// Add Age
 // Your task is to create a JavaScript function named addAge.
 // This function should take two parameters: an object and a number.
 // The function should add the provided number to the object as age and return the updated object.
-
-function addAge(obj, number) {
-    obj.age = number
-    return obj
-}
-
-console.log(addAge({'name':'Alice'},25))
-console.log(addAge({'name':'Bob','job':'coder'},51));
-
-// Your task is to create a JavaScript function named addAges.
-// This function should take two parameters: an array of objects and an array of numbers of equal length.
-// The function should add the numbers in the second array to the objects as age, following the corresponding index, and return the updated array of objects
-
-function addAges(objects, numbers) {
-let i = 0
-    for (let object of objects) {
-        object.age = numbers[i]
-        i = i + 1
+// function addAges(users,numbers) {
+//     let i = 0
+//     for(let user of users){
+//         user.age = numbers[i]
+//         i++
+//     }
+//     return users
+// }
+function addAges(users, numbers) {
+    for (let i = 0; i < users.length; i++) {
+        users[i].age = numbers[i]
     }
-    return objects
+    return users
 }
-
 console.log(addAges([{'name':'Alice'},{'name':'Bob'}],[21,51]))
 console.log(addAges([{'name':'Tom'},{'name':'Tim'},{'name':'Tam'}],[30,31,32]));
