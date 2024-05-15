a8a31df57c7a591ead38d59e0355e59b397ec715
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-15 17:09:30 +0200

    complete method kata3

--- methods_kata1.js
+++ methods_kata1.js
@@ -31,3 +31,28 @@ let greeter1 = {
 console.log(greeter1.sayHello1('John'))
 console.log(greeter1.sayHello1('Alice'))
 console.log(greeter1.sayHello1())
+
+// Counter
+// Create an object counter with 2 methods: increase and decrease.
+// Each method takes an object, as per the below tests, 
+// and returns the object with internal count value either increased by 1 or decreased by 1.
+
+// counter.increase({ count: 1 }) // { count: 2 }
+// counter.increase({ count: 10 }) // { count: 11 }
+// counter.decrease({ count: 1 }) // { count: 0 }
+// counter.decrease({ count: 10 }) // { count: 9 }
+
+let counter = {
+    increase: function(value) {
+        value.count += 1
+        return value
+    },
+    decrease: function(value) {
+        value.count -= 1
+        return value
+    }
+}
+console.log(counter.increase({ count: 1 }))
+console.log(counter.increase({ count: 10 }))
+console.log(counter.decrease({ count: 1 }))
+console.log(counter.decrease({ count: 10 }))
