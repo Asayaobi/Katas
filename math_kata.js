d5d50bc4065a32f57843f1ec1c5daaf5d09dbcb3
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-15 18:56:35 +0200

    complete math kata2

--- math_kata.js
+++ math_kata.js
@@ -20,6 +20,23 @@ console.log(findSmallestNumber(20,80,-5.5))
 console.log(findSmallestNumber(8,1))
 console.log(findSmallestNumber(2,0,5,3.9))
 
+// Correct Square Roots
+// Create a function checkSquareRoot that takes an array of objects.
+// Each object contains a value and a squareRoot property, where the squareRoot indicates the square root of the value.
+// If the square root is correct, add a property correct with a value of true to the object, otherwise false.
+// The function should then return the array of objects with their new correct field.
+// checkSquareRoot([{'value':100,'squareRoot':10},{'value':25,'squareRoot':6}])
 
+function checkSquareRoot(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].squareRoot === Math.sqrt(arr[i].value)) {
+            arr[i].correct = true
+        } else {
+            arr[i].correct = false
+        }
+    }
+    return arr
+}
 
-
+console.log(checkSquareRoot([{'value':100,'squareRoot':10},{'value':25,'squareRoot':6}]))
+console.log(checkSquareRoot([{'value':81,'squareRoot':9}])	)
