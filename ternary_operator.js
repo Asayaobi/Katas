200d9373742ff6d288030a4c125209e1bdfad355
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-16 17:49:52 +0200

    Complete ternary operator kata2

--- ternary_operator.js
+++ ternary_operator.js
@@ -34,3 +34,19 @@ function numberToBoolean(n) {
     return n > 0 ? true : false
 }
 console.log(numberToBoolean(-9))
+
+// Half or Double
+// Create a function halfOrDouble that takes an array of numbers.
+// The function should then check the value of each number and, if it's greater than 10, it should halve it, otherwise it should double it.
+// The function should then return the updated array.
+// Use a ternary operator.
+function halfOrDouble(numbers) {
+    let output =[]
+    for (let i = 0; i < numbers.length ; i++) {
+        numbers[i] > 10 ?output[i] = numbers[i]/2 : output[i] = numbers[i] * 2
+    }
+    return output
+}
+console.log(halfOrDouble([10,5,2,14]))
+console.log(halfOrDouble([-20,1,70,2,9,0,16]))
+console.log(halfOrDouble([]))
