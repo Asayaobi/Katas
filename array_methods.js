b5b5c9459d313cf46c8207aa4cf4ddaf16c14f1e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-16 19:42:49 +0200

    complete array method kata1

--- array_methods.js
+++ array_methods.js
@@ -87,3 +87,13 @@ console.log(newscore1)
 let namesA = ['Bob', 'Alice', 'John', 'Zelda']
 let newnames = namesA.reverse()
 console.log(newnames)
+
+// Return Length
+// Create a function returnLength that takes an array as its only parameter, 
+// then returns the length of the array as a number, or 'the array is empty' if there are no elements in the array.
+// returnLength([1,2,3,4])
+
+const returnLength = array => array.length === 0 ? 'the array is empty': array.length
+console.log(returnLength([1,2,3,4]))
+console.log(returnLength(['a']))
+console.log(returnLength([]))
