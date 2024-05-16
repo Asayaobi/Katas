f277c989a0309b04539e6bb6a1567fe161d1bb25
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-16 21:01:43 +0200

    complete string method kata1

--- string_method.js
+++ string_method.js
@@ -54,3 +54,15 @@ console.log(cities.split('-'))
 // Example
 '  Hello World  '.trim() // 'Hello World'
 console.log('  Hello World  '.trim() )
+
+// Return String Length
+// Create a returnLength function that takes a parameter and returns its length 
+// if it's a string. In any other case it returns the type of the parameter.
+const returnLength = random => {
+    return typeof random === 'string' ? random.length : typeof random
+}
+console.log(returnLength('hello'))
+console.log(returnLength(''))
+console.log(returnLength(10))
+console.log(returnLength([1,2,3]))
+console.log(returnLength(true))
