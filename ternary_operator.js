d51c30a9717ab3c5a7ea72b8c3aadce580f11daa
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-16 17:41:55 +0200

    Add ternary operator example

---
+++ ternary_operator.js
@@ -0,0 +1,14 @@
+// Ternary operator example
+
+let score = 85
+// let grade
+//     if (score >= 90) {
+//         grade = 'A'
+//     } else if (score > 75) {
+//         grade = 'B'
+//     } else {
+//         grade = 'C'
+//     }
+let grade = score >= 90 ? 'A' : score > 75 ? 'B' : 'c'
+
+console.log(grade)
