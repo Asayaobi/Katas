cfbe889413a22d28e26d0dc714b91c28c18927d3
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-16 20:48:25 +0200

    Add string method example 1

---
+++ string_method.js
@@ -0,0 +1,7 @@
+// 1. length
+// The length property provides the length of the string, 
+// i.e., the number of characters it contains. Just like in an array.
+// Example:
+const greeting = 'Hello, world!'
+greeting.length // 13
+console.log(greeting.length)
