48dd9050fe5c3ef9ddb6587bc9a80f6fd346895a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-16 18:29:32 +0200

    add example 4, 5 to ternary literals

--- template_literals.js
+++ template_literals.js
@@ -14,3 +14,12 @@ const age = 25
 const greeting = `Hello, my name is ${name} and I am ${age} years old`
 console.log(greeting) // Output: 'Hello, my name is Alice and I am 25 years old'
 
+// Embedded Expressions
+// Expressions embedded in template literals can be more than just simple variables. They can be complex operations or function calls.
+// Example:
+const result = `The sum of 2 and 3 is ${2 + 3}.`
+console.log(result) // 'The sum of 2 and 3 is 5.'
+
+const add = (a, b) => a + b
+const output = `The sum of 2 and 3 is ${add(2, 3)}.`
+console.log(output) 
