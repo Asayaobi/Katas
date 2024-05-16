bdd60498ffa98cad0c1a3727d26a994e2a12d6b7
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-16 22:31:36 +0200

    complete string method kata6

--- string_method.js
+++ string_method.js
@@ -107,3 +107,28 @@ const flipString = word => word.split('').reverse().join('')
 console.log(flipString('Hello'))
 console.log(flipString(''))
 console.log(flipString('Haiku Academy'))
+
+// Format Phone Number
+// A list of phone numbers was entered in the database as plain numbers, without proper formatting.
+// Create a function formatPhoneNumber that takes a 12 digit number and formats it according to the tests.
+// Assume the phone numbers never start with a zero.
+// If the number is not a number, if it's falsy or if it's not 12 digits, return the error message.
+const formatPhoneNumber = (numbers) => {
+    let format = '-'
+    if (typeof numbers !== 'number' || !numbers) {
+        return 'invalid number'
+    } else {
+        let stringNumbers = String(numbers)
+        if (stringNumbers.length !== 12) {
+            return 'invalid number'
+        } else {
+            let result = stringNumbers.slice(0,3) + '-' + stringNumbers.slice(3,6) + '-' + stringNumbers.slice(6,9) + '-' + stringNumbers.slice(9,12)
+            return result
+        }
+    }
+}
+console.log(formatPhoneNumber(123456789012))
+console.log(formatPhoneNumber(123))
+console.log(formatPhoneNumber(999999999999))
+console.log(formatPhoneNumber(783543777241))
+console.log(formatPhoneNumber(null))
