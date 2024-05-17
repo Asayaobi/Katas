193185421094ed1217407b2e2e92595242aab25f
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-17 21:17:43 +0200

    complete kata2 in array loop map

--- array_loops_map.js
+++ array_loops_map.js
@@ -64,3 +64,11 @@ console.log(doubledNumbers1([0, 100]))
 console.log(doubledNumbers1([]))
 
 
+// To Fahrenheit
+// Create a JavaScript function named convertToFahrenheit.
+// This function will take an array of temperatures in Celsius and return a new array with these temperatures converted to Fahrenheit.
+// The conversion formula to use is Fahrenheit = Celsius * 9 / 5 + 32.
+// Use the map method.
+const convertToFahrenheit = temp => temp.map(t => t * 9 / 5 +32)
+console.log(convertToFahrenheit([15,40,12,5]))
+console.log(convertToFahrenheit([42,0,-10]))
