fee51fbf7a6294940a3b674a15ac5f3678a6e106
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-07-28 20:49:32 +0200

    complete codewars kata1

--- codewars.js
+++ codewars.js
@@ -163,3 +163,25 @@ console.log(removeDuplicates([1, 2, 3, 4, 4, 5, 6, 6]));
 // javascript
 // Copy code
 // commonElements([1, 2, 3, 4], [3, 4, 5, 6]); // Output: [3, 4]
+
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. 
+// Essentially, rearrange the digits to create the highest possible number.
+// Examples:
+// Input: 42145 Output: 54421
+// Input: 145263 Output: 654321
+// Input: 123456789 Output: 987654321
+
+// let number = 12345
+// let digitArray = Array.from(String(number), Number)
+// console.log(digitArray)
+// let sort = digitArray.sort((a, b) => b - a)
+// console.log(sort.join(''))
+
+// function descendingOrder(input){
+//     let result = Array.from(String(input), Number).sort((a, b) => b - a).join('')
+//     return Number(result)
+//   }
+
+  let sortNumber = (n) => (Number(Array.from(String(n), Number).sort((a, b) => b - a).join('')))
+  console.log(sortNumber(42145))
