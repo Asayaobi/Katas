64e0aa5a422f7ac3ff25d1386e20ae1d53312743
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-07-28 23:16:14 +0200

    complete codewars kata2

--- codewars.js
+++ codewars.js
@@ -185,3 +185,35 @@ console.log(removeDuplicates([1, 2, 3, 4, 4, 5, 6, 6]));
 
   let sortNumber = (n) => (Number(Array.from(String(n), Number).sort((a, b) => b - a).join('')))
   console.log(sortNumber(42145))
+
+//   An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+//   Implement a function that determines whether a string that contains only letters is an isogram. 
+//   Assume the empty string is an isogram. Ignore letter case.
+// Example: (Input --> Output)
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+function isIsogram(str){
+    str = str.toLowerCase()
+    for(let i = 0; i < str.length; i++)
+      for(let j = i + 1; j < str.length; j++)
+        if(str[i] === str[j])
+          return false
+    return true
+  }
+
+// let isIsogram = str => {
+//     let wordArray = Array.from(String(str.toLowerCase()))
+//     for (i = 0; i < wordArray.length; i++) {
+//    let wordsMinus = wordArray.slice(i+1)
+//    if (wordsMinus.includes(wordArray[i])) {
+//     return false
+//    } 
+// }
+// return true
+// } 
+
+console.log(isIsogram("aba"))
+console.log(isIsogram("Dermatoglyphics"))
+console.log(isIsogram("moOse"))
