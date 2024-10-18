69bc64f3cd1c36dcf16f3c7cd9dfebd88348287b
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-10-18 11:43:44 +0200

    add new kata

--- codewars.js
+++ codewars.js
@@ -279,3 +279,27 @@ let comp = (array1, array2) => {
 console.log(comp([121, 144, 19, 161, 19, 144, 19, 11],[121, 14641, 20736, 361, 25921, 361, 20736, 361]))
 console.log(comp([121, 144, 19, 161, 19, 144, 19, 11],[132, 14641, 20736, 361, 25921, 361, 20736, 361]))
 console.log(comp([121, 144, 19, 161, 19, 144, 19, 11],[121, 14641, 20736, 36100, 25921, 361, 20736, 361]))
+
+//Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer.
+
+//Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, 
+// starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) 
+// and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+// Example:
+// "MM"      -> 2000
+// "MDCLXVI" -> 1666
+// "M"       -> 1000
+// "CD"      ->  400
+// "XC"      ->   90
+// "XL"      ->   40
+// "I"       ->    1
+
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
