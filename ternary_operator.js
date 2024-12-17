f1fc79c1566bbffe1c0180894b0b1beed1559d84
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-12-17 14:08:49 +0100

    add more example to ternary operator

--- ternary_operator.js
+++ ternary_operator.js
@@ -56,3 +56,17 @@ function halfOrDouble(numbers) {
 console.log(halfOrDouble([10,5,2,14]))
 console.log(halfOrDouble([-20,1,70,2,9,0,16]))
 console.log(halfOrDouble([]))
+
+//Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. 
+//In his country, it's usual to tip 15% if the bill value is between 50 and 300. 
+//If the value is different, the tip is 20%.
+//It's not allowed to use an if...else statement 
+//Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+
+//Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+
+
+const bill = 275;
+const tip = bill >= 50 || bill <=300 ? bill * 0.15 : bill * 0.2
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill+tip}`)
