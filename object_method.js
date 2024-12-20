9349e1e3caa6001ffa4b563d35564186cc43df85
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-12-20 14:09:36 +0100

    add new kata to object method.js

--- object_method.js
+++ object_method.js
@@ -61,3 +61,46 @@ console.log(jonas.age) //34
 // console.log(jonas['calcAge'](1998)) //27
 
 console.log(jonas.getDetail()) //'Jonas is a 34 years old teacher with a dricer license.'
+
+
+//Let's go back to Mark and John comparing their BMIs!
+//Remember: BMI = mass / (height * height) (mass in kg and height in meters).
+
+//Your tasks:
+
+//For each of them, create an object with properties for their full name, mass, and height 
+//(Mark Miller and John Smith). Name these objects as mark and john, and their properties exactly as fullName, mass and height.
+
+//Create a calcBMI method on each object to calculate the BMI (the same method on both objects). 
+// Assign the BMI value to a property called bmi (lowercase), and also return it from the method.
+
+//Log to the console who has the higher BMI, together with the full name and the respective BMI. 
+// Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+
+//TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+//Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+
+const markMiller = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function(){
+        this.bmi = this.mass / (this.height * this.height)
+        return this.bmi
+    }
+}
+
+const johnSmith = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function(){
+        this.bmi = this.mass / (this.height * this.height)
+        return this.bmi
+    }
+}
+
+console.log(`${markMiller.fullName}'s BMI (${markMiller.calcBMI()}) is higher than ${johnSmith.fullName}'s (${johnSmith.calcBMI()})!`)
+// console.log(markMiller.calcBMI())//27.30
+// console.log(johnSmith.calcBMI())//24.19
