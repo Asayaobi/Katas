b124793b54268617f2e365094dfa3b70c2c69a07
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-12-20 13:33:27 +0100

    add function to object method

--- object_method.js
+++ object_method.js
@@ -1,8 +1,11 @@
 const jonas = {
     firstName: 'Jonas',
     lastName: 'Schmedtmann',
-    age: 2025-1991,
-    job: 'teacher'
+    job: 'teacher',
+    //adding function
+    calcAge: function (birthYear){
+        return 2025 - birthYear
+    }
 }
 
 //using dot vs bracket notation to access object data
@@ -12,14 +15,14 @@ console.log(jonas['lastName']) //'Schmedtmann'
 const nameKey = 'Name'
 console.log(jonas['first' + nameKey]) //Jonas
 
-// const answer = prompt('what do you want to know about Jonas? age, firstName, lastName, job?') //type job
+// const answer = prompt('what do you want to know about Jonas? firstName, lastName, job?') //type job
 // console.log(jonas[answer]) // 'teacher'
 
 // if (jonas[answer]){
 //     console.log(jonas[answer])
 // } else {
 //     //in case answer is undefined
-//     console.log('Please choose between age, firstName, lastName, job')
+//     console.log('Please choose between firstName, lastName, job')
 // }
 
 //adding data to the object
@@ -27,3 +30,7 @@ jonas.location = 'Portugal'
 jonas['nickName'] = 'Jo'
 
 console.log(jonas)
+
+//calling the function from the object
+console.log(jonas.calcAge(1991)) //34
+console.log(jonas['calcAge'](1998)) //27
