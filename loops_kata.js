03251082c9be0e09f436a0391f8241f550b10ccd
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-12-23 17:48:39 +0100

    add more kata to loops kata

--- loops_kata.js
+++ loops_kata.js
@@ -86,3 +86,61 @@ while (dice !== 6){
         console.log('Loop is about to end..')
     }
 }
+
+//Your tasks:
+//Create an array called bills containing all 10 test bill values.
+//Create empty arrays for the tips and the totals (tips and totals)
+//Use the calcTip function we wrote before (included in the starter code) 
+// to calculate tips and total values (bill + tip) for every bill value in the bills array. 
+// Use a for loop to perform the 10 calculations!
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   }
+
+
+//TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52.
+
+
+
+//BONUS:
+//Write a function calcAverage which takes an array called arr as an argument. 
+// This function calculates the average of all numbers in the given array. 
+
+//First, you will need to add up all values in the array. To do the addition, 
+// start by creating a variable sum that starts at 0. Then loop over the array using a for loop. 
+
+// In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+//To calculate the average, divide the sum you calculated before by the length of the array 
+// (because that's the number of elements).
+
+//Call the function with the totals array.
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+let tips = []
+let totals = []
+
+const calcTip = bills => {
+    for (let i = 0; i < bills.length; i++){
+        if (bills[i] >= 50 && bills[i] <= 300){
+            tips[i] = bills[i] * 0.15
+            totals[i] = bills[i] + (bills[i] * 0.15)
+        } else {
+            tips[i] = bills[i] * 0.2
+            totals[i] = bills[i] + (bills[i] * 0.2)
+        }
+    }
+}
+
+
+const calcAverage = arr => {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++){
+        sum += bills[i]
+    }
+    let average = sum / arr.length
+    return average
+}
+
+console.log(calcAverage(bills))
+
