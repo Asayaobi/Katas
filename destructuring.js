c430cd0e391dad392699e0867b7156c29102ed69
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-16 17:08:06 +0100

    add kata9, 10

--- destructuring.js
+++ destructuring.js
@@ -434,3 +434,43 @@ console.log(programmingLanguage)//'unknown'
 let bookTitle = 'unknown';
 let bookAuthor = 'unknown';
 ({title: bookTitle, author: bookAuthor} = books[0])
+
+/*9.Each book object has a deeply nested rating property as illustrated below:
+{
+    title: 'Algorithms',
+    ...
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,              // <-- HERE
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13
+      }
+    }
+  },
+Destructure the first book object from the books array into a variable called bookRating. In the result of your destructuring, 
+the bookRating variable should be assigned with the value of the book[0].thirdParty.goodreads.rating property.
+
+Please do most of the work on the left side of the assignment operator: const ... = books[0]*/
+const {thirdParty:{goodreads: {rating:bookRating}}} =  books[0]
+console.log(bookRating) //4.41
+
+/*10. Write a function called printBookInfo that has three parameters called title, author and year. 
+This function should work for a single object passed as an argument, and it should log to the console information
+about the book in this format: "${title} by ${author}, ${year}".
+
+If year is undefined (was not passed), it should be assigned with a default value of 'year unknown'.
+
+Code:
+printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick', year: '2011' });
+Expected output:
+"Algorithms by Robert Sedgewick, 2011"
+*/
+
+function printBookInfo({title, author, year = 'year unknown'}){
+    return `${title} by ${author}, ${year}`
+}
+console.log(printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick', year: '2011' }))//Algorithms by Robert Sedgewick, 2011
+console.log(printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick' }))//"Algorithms by Robert Sedgewick, year unknown"
+
