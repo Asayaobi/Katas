df10a7e2e8fadb20a9df60cd3a373331ae60e948
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-18 18:34:00 +0100

    add kata1

--- logicalops_kata.js
+++ logicalops_kata.js
@@ -26,6 +26,84 @@ if (restaurant.orderPizza){
 restaurant.orderPizza && restaurant.orderPizza('cheese', 'onion')
 
 
+/* Kata
+
+Some of the book objects have the programmingLanguage property, 
+which specifies what programming language is used in the book, for example
+{
+  title: 'Algorithms',
+  author: ['Robert Sedgewick', 'Kevin Wayne'],
+  ...
+  programmingLanguage: 'Java',     // <-- HERE
+}
+
+Write a function called hasExamplesInJava that takes a book object from the books array as an argument. 
+This function should return true if the book uses Java, 
+or a string 'no data available' if it uses other language or no programming language at all.
+
+Use short-circuiting.
+
+Example 1 Code: hasExamplesInJava(books[0]); //True
+Example 2 Code: hasExamplesInJava(books[1]); //"no data available"
+*/ 
+const books = [
+    {
+      title: 'Algorithms',
+      author: ['Robert Sedgewick', 'Kevin Wayne'],
+      publisher: 'Addison-Wesley Professional',
+      publicationDate: '2011-03-24',
+      edition: 4,
+      keywords: ['computer science', 'programming', 'algorithms', 'data structures', 'java', 'math', 'software', 'engineering'],
+      pages: 976,
+      format: 'hardcover',
+      ISBN: '9780321573513',
+      language: 'English',
+      programmingLanguage: 'Java',
+      onlineContent: true,
+      thirdParty: {
+        goodreads: {
+          rating: 4.41,
+          ratingsCount: 1733,
+          reviewsCount: 63,
+          fiveStarRatingCount: 976,
+          oneStarRatingCount: 13
+        }
+      },
+      highlighted: true
+    },
+    {
+      title: 'Structure and Interpretation of Computer Programs',
+      author: ['Harold Abelson', 'Gerald Jay Sussman', 'Julie Sussman (Contributor)'],
+      publisher: 'The MIT Press',
+      publicationDate: '2022-04-12',
+      edition: 2,
+      keywords: ['computer science', 'programming', 'javascript', 'software', 'engineering'],
+      pages: 640,
+      format: 'paperback',
+      ISBN: '9780262543231',
+      language: 'English',
+      programmingLanguage: 'JavaScript',
+      onlineContent: false,
+      thirdParty: {
+        goodreads: {
+          rating: 4.36,
+          ratingsCount: 14,
+          reviewsCount: 3,
+          fiveStarRatingCount: 8,
+          oneStarRatingCount: 0
+        }
+      },
+      highlighted: true
+    },
+]
+
+function hasExamplesInJava(bookObj){
+    return bookObj.programmingLanguage === 'Java' || "no data available"
+}
+
+console.log(hasExamplesInJava(books[0]))//True
+console.log(hasExamplesInJava(books[1]))//"no data available"
+
 // Simplifying Nested If Statements
 // You have a piece of JavaScript code that uses nested if statements. 
 // Your task is to refactor this code into a single if statement using the correct logical operator 
