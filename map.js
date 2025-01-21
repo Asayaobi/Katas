d5d1a595d7fdd17bb051b4d8de40b557ccaf906e
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-21 16:45:11 +0100

    add katas to map

--- map.js
+++ map.js
@@ -337,3 +337,35 @@ const books = [
 //1. Convert the first book object from the books array into a Map, and assign it to a firstBookMap variable.
 const firstBookMap = new Map(Object.entries(books[0]))
 console.log(firstBookMap)
+/* Map {
+  'title': 'Algorithms',
+  'author': [ 'Robert Sedgewick', 'Kevin Wayne' ],
+  'publisher': 'Addison-Wesley Professional',
+  'publicationDate': '2011-03-24',
+  'edition': 4,
+  'keywords': Array(8) [
+    'computer science', 'programming', 'algorithms', 'data structures', 'java', 'math',
+    'software', 'engineering'
+  ],
+  'pages': 976,
+  'format': 'hardcover',
+  'ISBN': '9780321573513',
+  'language': 'English',
+  'programmingLanguage': 'Java',
+  'onlineContent': true,
+  'thirdParty': {
+    goodreads: {
+      rating: 4.41,
+      ratingsCount: 1733,
+      reviewsCount: 63,
+      fiveStarRatingCount: 976,
+      oneStarRatingCount: 13
+    }
+  },
+  'highlighted': true
+} */
+
+//2.Use the for-of loop to iterate over firstBookMap, and log to the console keys that have numbers as values.
+for (const [key, value] of firstBookMap){
+    if (typeof value === 'number') console.log(key) //'edition', 'pages'
+}
