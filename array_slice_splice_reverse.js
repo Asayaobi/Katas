112677bb7f633d6bb80b559b1cf5475679808aa3
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-28 14:12:27 +0100

    add splice method

--- array_slice_splice_reverse.js
+++ array_slice_splice_reverse.js
@@ -9,3 +9,11 @@ console.log(arr.slice(-2))//[ 'c', 'd' ]
 //create a copy of any array
 console.log(arr.slice())//['a','b','c','d']
 console.log([...arr])//['a','b','c','d']
+
+//SPLICE METHOD: modify the original array by deleting the splice part out of the array
+const arr1 = ['a','b','c','d','e']
+
+console.log(arr1.splice(-1))//[ 'e' ]
+console.log(arr)//[ 'a', 'b', 'c' ,'d']
+console.log(arr.splice(1,2)) //delete the first 2 items after the 1st item in the array.//[ 'b', 'c' ]
+console.log(arr)//[ 'a', 'd' ]
