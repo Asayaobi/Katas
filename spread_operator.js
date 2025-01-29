fdaced4e0295966a36b6557185d0d5cf03fd6d6a
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-01-29 12:33:25 +0100

    remove indexOf.js

--- spread_operator.js
+++ spread_operator.js
@@ -293,3 +293,13 @@ function printBookAuthorsCount(title,...authors){
   return `"The book "${title}" has ${authors.length} authors`
 }
 console.log(printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne'))//The book "Algorithms" has 2 authors
+
+
+//write a function that takes an array of numbers and return the index of the largest number
+function getLargestNumberIndex(arr){
+  const largestNumber = Math.max(...arr)
+  const output = arr.indexOf(largestNumber)
+  return output
+}
+
+console.log(getLargestNumberIndex([3,6,12,8,0,1])) //2
