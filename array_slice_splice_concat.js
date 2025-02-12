6312e7afdd0a77406e82dff2998c89d67f7fda66
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-02-12 11:36:23 +0100

    add toSpliced method

--- array_slice_splice_concat.js
+++ array_slice_splice_concat.js
@@ -14,9 +14,16 @@ console.log([...arr])//['a','b','c','d','e']
 const arr1 = ['a','b','c','d','e']
 
 console.log(arr1.splice(-1))//[ 'e' ]
-console.log(arr)//[ 'a', 'b', 'c' ,'d']
-console.log(arr.splice(1,2)) //delete the first 2 items after the 1st item in the array.//[ 'b', 'c' ]
-console.log(arr)//[ 'a', 'd' ]
+console.log(arr1)//[ 'a', 'b', 'c' ,'d']
+console.log(arr1.splice(1,2)) //delete the first 2 items after the 1st item in the array.//[ 'b', 'c' ]
+console.log(arr1)//[ 'a', 'd' ]
+
+// Splice() modify the original array
+// toSpliced() creates a new copy of the original array
+const originalArr = ['a','b','c','d','e']
+const newArr = originalArr.toSpliced(2,1) //after position2, delete 1 element
+console.log(newArr)//[ 'a', 'b', 'd', 'e' ]
+
 
 //REVERSE METHOD: modify the original array
 const arr2 = [1,2,3,4,5]
