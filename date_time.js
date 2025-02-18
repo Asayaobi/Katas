f73c71259431bc594e9004ade43d0bf0950bfbbc
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-02-18 11:24:01 +0100

    add International format API

--- date_time.js
+++ date_time.js
@@ -37,3 +37,23 @@ const day2 = new Date(2024, 1, 16)
 //Math.abs() to avoid the case when day 1 is more than day 2 -> the result will be negative and it won't work
 const calcDayPass = (day1, day2) => Math.abs(day2 - day1) / (1000 * 60 *60 * 24) //if there's hr + mins then add Math.round() to round up to one day instead of x.xx
 console.log(calcDayPass(day1,day2))//2 days
+
+//Display Date 
+const now = new Date()
+//mannually define locale with ISO Language Code Table -> 'en-US'2/18/2025, 'en-GB'2/18/2025
+//new Intl.DateTimeFormat('en-US').format(now) //2/18/2025
+
+//use options to display hours too
+const options = {
+  hour: 'numeric',
+  minute: 'numeric',
+  day: 'numeric',
+  month: 'long', //it can be numberic, 2-digit
+  year: 'numeric',
+  weekday: 'long'
+}
+//new Intl.DateTimeFormat('en-GB',options).format(now) //Tuesday 18 February 2025 at 11:01 AM
+
+//getting user's locale
+const locale = navigator.language//en-US
+console.log(new Intl.DateTimeFormat(locale,options).format(now))  //Tuesday, February 18, 2025 at 11:01 AM
