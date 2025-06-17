2df0c816f31f3a9f383ed8add65d6cfc4e9f8fc0
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-17 22:13:30 +0200

    update bmi challenge

--- codewars.js
+++ codewars.js
@@ -600,3 +600,11 @@ console.log(squareDigits(3212))//9414
 // Your challenge is to create a function that takes weight and height as inputs and gives the calculated BMI value as an output. 
 // The output should be rounded to the nearest whole number.
 // The first parameter should be the weight and the second should be the height.
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+var bmi = bmiCalculator(65, 1.8); 
+bmi should equal 20 when it's rounded to the nearest whole number.
+*/
+
+const bmiCalculator = (weight, height) => Math.round(weight/(height ** 2))
+console.log(bmiCalculator(65, 1.8)) //20
+
