14ee2d413b721310c76851dbdce6e583df818984
Asaya <145620779+Asayaobi@users.noreply.github.com>
2025-06-18 15:21:29 +0200

    add leap year challenge

--- codewars.js
+++ codewars.js
@@ -629,3 +629,26 @@ const bmiCalculatorAdvance = (weight, height) => {
 }
 
 console.log(bmiCalculatorAdvance(40, 1.8))//'Your BMI is 12, so you are underweight.'
+
+
+// Leap Year Challenge Exercise
+// Write a program that works out whether if a given year is a leap year. 
+// A normal year has 365 days, leap years have 366, with an extra day in February. 
+
+// This is how to work out whether if a particular year is a leap year:
+// A year is a leap year if it is evenly divisible by 4 ;
+// except if that year is also evenly divisible by 100;
+// unless that year is also evenly divisible by 400.
+
+
+// e.g. Is the year 2000 a leap year?:
+// 2000 ÷ 4 = 500 (Leap)
+// 2000 ÷ 100 = 20 (Not Leap)
+// 2000 ÷ 400 = 5 (Leap!)
+// So the year 2000 is a leap year.
+
+// But the year 2100 is not a leap year because:
+// 2100 ÷ 4 = 525 (Leap)
+// 2100 ÷ 100 = 21 (Not Leap)
+// 2100 ÷ 400 = 5.25 (Not Leap)
+
